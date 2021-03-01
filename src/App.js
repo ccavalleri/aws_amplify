a858fca9179a8f69f8ca7618861f0f27348652b4
bacetech <ciro.cavalleri@gmail.com>
2021-03-01 18:17:32 +0100

    showing app is running on AWS Amplify

--- src/App.js
+++ src/App.js
@@ -9,12 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <h1> Hello from AWS Amplify</h1>
           Learn React
         </a>
       </header>
